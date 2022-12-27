6c26b86d730071d70a23eb3a08686fa9e0770529
Saheen Shoukath <saheenshoukath@gmail.com>
2022-12-27 17:55:49 +0530

    add scroll

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from "react";
 
 const Container: React.FC<PropsWithChildren> = ({ children }) => {
-  return <div className="bg-primary h-screen">{children}</div>;
+  return <div className="bg-primary h-screen overflow-y-auto">{children}</div>;
 };
 
 export default Container;
